8f2ccfcd8174547e5835a1de9dc659e20f4bcefb
Matheus da Cruz <matheuswachcruz@gmail.com>
2023-07-06 15:49:04 -0300

    feat: finish order component structure finalized

--- src/pages/Checkout/components/FinishOrder/index.tsx
+++ src/pages/Checkout/components/FinishOrder/index.tsx
@@ -1,7 +1,12 @@
+import { Trash } from "phosphor-react";
+import { IncreaseDecreaseAmountButtons } from "../../../../components/IncreaseDecreaseAmountButtons";
+import { coffeeList } from "../../../../utils/data/coffee-list";
+
 import {
   FinishOrderContainer,
   CoffeesListContainer,
   CoffeeItem,
+  RemoveCoffeeFromOrderButton,
   TotalOrderAmountContainer,
   FinishOrderButton,
 } from "./styles";
@@ -10,18 +15,42 @@ export function FinishOrder() {
   return (
     <FinishOrderContainer>
       <CoffeesListContainer>
-        <CoffeeItem>
-
-        </CoffeeItem>
+        {coffeeList?.slice(0, 2)?.map((coffee) => (
+          <CoffeeItem key={coffee?.id}>
+            <div>
+              <img src={coffee?.src} alt={coffee?.type} />
+              <div>
+                <span>{coffee?.type}</span>
+                <section>
+                  <IncreaseDecreaseAmountButtons coffee={coffee} />
+                  <RemoveCoffeeFromOrderButton type="button">
+                    <Trash size={16} />
+                    Remove
+                  </RemoveCoffeeFromOrderButton>
+                </section>
+              </div>
+            </div>
+            <strong>${coffee?.price}</strong>
+          </CoffeeItem>
+        ))}
       </CoffeesListContainer>
 
       <TotalOrderAmountContainer>
-
+        <section>
+          <span>Total items</span>
+          <span>$3.3</span>
+        </section>
+        <section>
+          <span>Delivery</span>
+          <span>$4</span>
+        </section>
+        <section>
+          <strong>Total</strong>
+          <strong>$7.3</strong>
+        </section>
       </TotalOrderAmountContainer>
 
-      <FinishOrderButton>
-        Finish order
-      </FinishOrderButton>
+      <FinishOrderButton>Finish order</FinishOrderButton>
     </FinishOrderContainer>
   );
 }
--- src/pages/Checkout/components/FinishOrder/styles.ts
+++ src/pages/Checkout/components/FinishOrder/styles.ts
@@ -14,11 +14,125 @@ export const FinishOrderContainer = styled.div`
 export const CoffeesListContainer = styled.ul`
   list-style: none;
   max-width: 23rem;
+  display: flex;
+  flex-direction: column;
+  gap: 1.5rem;
 `;
 
-export const CoffeeItem = styled.li``;
+export const CoffeeItem = styled.li`
+  padding: 0.5rem 0.25rem 1.5rem 0.25rem;
+  border-bottom: 1px solid ${(props) => props.theme["gray-400"]};
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 3.125rem;
 
-export const TotalOrderAmountContainer = styled.div``;
+  & > div {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 1.25rem;
+
+    img {
+      width: 4rem;
+      height: 4rem;
+    }
+
+    & > div {
+      display: flex;
+      flex-direction: column;
+      gap: 0.5rem;
+
+      span {
+        font-size: 1rem;
+        line-height: 130%;
+        color: ${(props) => props.theme["gray-800"]};
+      }
+
+      section {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        gap: 0.5rem;
+
+        & > div {
+          width: 4.5rem;
+          height: 2.125rem;
+        }
+      }
+    }
+  }
+
+  strong {
+    align-self: flex-start;
+    font-size: 1rem;
+    line-height: 130%;
+    font-weight: 700;
+    color: ${(props) => props.theme["gray-700"]};
+  }
+`;
+
+export const RemoveCoffeeFromOrderButton = styled.button`
+  width: 5.625rem;
+  height: 2.125rem;
+  padding: 0.5rem;
+  border-radius: 0.375rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.25rem;
+  cursor: pointer;
+  border: 0;
+  background-color: ${(props) => props.theme["gray-400"]};
+  color: ${(props) => props.theme["gray-700"]};
+  transition: all 0.2s ease-in-out;
+  text-transform: uppercase;
+  font-size: 0.75rem;
+  line-height: 160%;
+
+  &:focus {
+    outline: 0;
+    box-shadow: 0 0 0 2px ${(props) => props.theme["purple-400"]};
+  }
+
+  &:hover {
+    opacity: 0.9;
+  }
+
+  svg {
+    color: ${(props) => props.theme["purple-400"]};
+  }
+`;
+
+export const TotalOrderAmountContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 0.75rem;
+
+  section {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    gap: 0.5rem;
+    line-height: 130%;
+    color: ${(props) => props.theme["gray-700"]};
+
+    span:nth-child(1) {
+      font-size: 0.875rem;
+    }
+
+    span:nth-child(2) {
+      font-size: 1rem;
+    }
+
+    strong {
+      font-size: 1.25rem;
+      font-weight: 700;
+      color: ${(props) => props.theme["gray-800"]};
+    }
+  }
+`;
 
 export const FinishOrderButton = styled.button`
   width: 100%;
