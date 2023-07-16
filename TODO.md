a81859ddbc874e6ab36ac59f81b31a1fad8aa646
Matheus da Cruz <matheuswachcruz@gmail.com>
2023-07-15 21:12:22 -0300

    feat: get user location and show it in the header

--- TODO.md
+++ TODO.md
@@ -9,6 +9,6 @@ Features
 [x] Aumentar ou diminuir a quantidade de cafés no carrinho
 [x] Exibir o total de itens no carrinho no Header
 [x] Exibir o valor total da soma de itens no carrinho multiplicados pelo valor
-[] Pedir permissão para capturar localização do usuário. Se ele aceitar, mostrar cidade e estado no canto superior direito
+[x] Pedir permissão para capturar localização do usuário. Se ele aceitar, mostrar cidade e estado no canto superior direito
 [] Encontrar uma api para completar dados de endereço baseados em um CEP
 [] Mostrar popup messages após algumas ações realizadas
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 import { MapPin, ShoppingCart } from "phosphor-react";
 
@@ -5,9 +6,45 @@ import { useCoffeeOrder } from "../../hooks/useCoffeeOrder";
 
 import logo from "../../assets/images/logo.svg";
 import { HeaderContainer, TotalCoffeesInTheCartBadge } from "./styles";
+import { api } from "../../services/api";
 
 export function Header() {
   const { order } = useCoffeeOrder();
+
+  const [userLocation, setUserLocation] = useState<string | null>(null);
+
+  async function getUserLocation({
+    coords,
+  }: GeolocationPosition): Promise<void> {
+    const { latitude, longitude } = coords;
+
+    try {
+      const { data } = await api.get(
+        `${
+          import.meta.env.VITE_OPEN_CAGE_GEOCODING_URI
+        }json?q=${latitude}+${longitude}&key=${
+          import.meta.env.VITE_OPEN_CAGE_GEOCODING_API_KEY
+        }`
+      );
+
+      const { town, state_code } = data.results[0].components;
+
+      setUserLocation(`${town}, ${state_code}`);
+    } catch (error) {
+      console.log({ error });
+    }
+  }
+
+  function getUserCurrentPosition() {
+    if (window.navigator.geolocation) {
+      window.navigator.geolocation.getCurrentPosition(getUserLocation);
+    }
+  }
+
+  useEffect(() => {
+    getUserCurrentPosition();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <HeaderContainer>
@@ -19,14 +56,18 @@ export function Header() {
       </NavLink>
 
       <div>
-        <div>
-          <MapPin weight="fill" size={22} />
-          <span>Venâncio Aires, RS</span>
-        </div>
+        {!!userLocation && (
+          <div>
+            <MapPin weight="fill" size={22} />
+            <span>{userLocation}</span>
+          </div>
+        )}
         <NavLink to="/checkout" title="See cart">
           <ShoppingCart weight="fill" size={22} />
           {order?.coffeesAmount > 0 && (
-            <TotalCoffeesInTheCartBadge title={`${order?.coffeesAmount} coffees added`}>
+            <TotalCoffeesInTheCartBadge
+              title={`${order?.coffeesAmount} coffees added`}
+            >
               <strong>{order?.coffeesAmount}</strong>
             </TotalCoffeesInTheCartBadge>
           )}
